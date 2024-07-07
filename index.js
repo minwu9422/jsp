c17ded0969186bf787c60c3b7d6f704ae023945b
minwu9422 <minwu9422@gmail.com>
2024-07-07 11:17:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const tls = require('tls');
 const net = require('net');
+const WebSocket = require('ws');
 
 const LOCAL_PORT = 30303;
-const REMOTE_HOST = '172.64.102.151';
-const REMOTE_PORT = 443;
+const REMOTE_WS_URL = 'wss://www.speedtest.net'; 
 
-
-const tlsOptions = {
-    host: REMOTE_HOST,
-    port: REMOTE_PORT,
-    rejectUnauthorized: false // 
-};
-const remoteSocket = tls.connect(tlsOptions);
+const remoteSocket = new WebSocket(REMOTE_WS_URL);
 
 const server = net.createServer((localSocket) => {
-    localSocket.on('data', (data) => {
-        remoteSocket.write(data);
-    });
+    remoteSocket.on('open', () => {
+        localSocket.on('data', (data) => {
+            remoteSocket.send(data);
+        });
 
-    remoteSocket.on('data', (data) => {
-        localSocket.write(data);
-    });
+        remoteSocket.on('message', (data) => {
+            localSocket.write(data);
+        });
 
-    localSocket.on('close', () => {
-        remoteSocket.end();
-    });
+        localSocket.on('close', () => {
+            remoteSocket.close();
+        });
 
-    remoteSocket.on('close', () => {
-        localSocket.end();
-    });
+        remoteSocket.on('close', () => {
+            localSocket.end();
+        });
 
-    localSocket.on('error', (err) => {
-        console.error('Local socket error:', err.message);
-        localSocket.destroy();
-        remoteSocket.end();
+        localSocket.on('error', (err) => {
+            console.error('Local socket error:', err.message);
+            localSocket.destroy();
+            remoteSocket.close();
+        });
+
+        remoteSocket.on('error', (err) => {
+            console.error('Remote socket error:', err.message);
+            remoteSocket.close();
+            localSocket.end();
+        });
     });
 
     remoteSocket.on('error', (err) => {
-        console.error('Remote socket error:', err.message);
-        remoteSocket.destroy();
-        localSocket.end();
+        console.error('WebSocket connection error:', err.message);
+        localSocket.destroy();
     });
 });
 
@@ -50,4 +50,3 @@ server.listen(LOCAL_PORT, () => {
 server.on('error', (err) => {
     console.error('Server error:', err.message);
 });
-
