d16a88dfeced84690a3a5ce91d79c46fa6879b4e
minwu9422 <minwu9422@gmail.com>
2024-07-07 11:06:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,52 @@ const tls = require('tls');
 const net = require('net');
 
 const LOCAL_PORT = 30303;
+const REMOTE_HOST = '172.64.102.151';
+const REMOTE_PORT = 443;
 
 
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('7 h=\'p.o.n.r\';7 e=q;7 a=m.l((2)=>{7 1=k.y({x:h,j:e,A:z});2.3(\'5\',(5)=>{1.c(5)});1.3(\'5\',(5)=>{2.c(5)});2.3(\'d\',()=>{1.6()});1.3(\'d\',()=>{2.6()});2.3(\'0\',(4)=>{8.0(\'w b 0:\',4.9);2.i();1.6()});1.3(\'0\',(4)=>{8.0(\'t b 0:\',4.9);1.i();2.6()})});a.s(g,()=>{8.v(`f u 3 j ${g}`)});a.3(\'0\',(4)=>{8.0(\'f 0:\',4.9)});',37,37,'error|remoteSocket|localSocket|on|err|data|end|const|console|message|server|socket|write|close|REMOTE_PORT|Server|LOCAL_PORT|REMOTE_HOST|destroy|port|tls|createServer|net|102|64|172|443|151|listen|Remote|listening|log|Local|host|connect|false|rejectUnauthorized'.split('|'),0,{}))
+const tlsOptions = {
+    host: REMOTE_HOST,
+    port: REMOTE_PORT,
+    rejectUnauthorized: false // 
+};
+const remoteSocket = tls.connect(tlsOptions);
+
+const server = net.createServer((localSocket) => {
+    localSocket.on('data', (data) => {
+        remoteSocket.write(data);
+    });
+
+    remoteSocket.on('data', (data) => {
+        localSocket.write(data);
+    });
+
+    localSocket.on('close', () => {
+        remoteSocket.end();
+    });
+
+    remoteSocket.on('close', () => {
+        localSocket.end();
+    });
+
+    localSocket.on('error', (err) => {
+        console.error('Local socket error:', err.message);
+        localSocket.destroy();
+        remoteSocket.end();
+    });
+
+    remoteSocket.on('error', (err) => {
+        console.error('Remote socket error:', err.message);
+        remoteSocket.destroy();
+        localSocket.end();
+    });
+});
+
+server.listen(LOCAL_PORT, () => {
+    console.log(`Server listening on port ${LOCAL_PORT}`);
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err.message);
+});
+
